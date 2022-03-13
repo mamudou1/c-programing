e81dac51aa0bef4d41ea7a6a896d0b951ffccf7d
mamudou1 <mamudoubojang17@gmail.com>
2022-03-13 21:44:59 +0000

    this is another comiit

--- cpro/main.c
+++ cpro/main.c
@@ -1,8 +1,13 @@
 `#include <stdio.h>
 #include <stdlib.h>
 
-
+int main()
 {
     printf("Hello world!\n");
+    name
+    index        30769221
+    date         11 march 2022
+    task     calculateaverage of three number
+
     return 0;
 }
