5135f735da4dd75859aab537e00b8dc309f3b05b
mamudou1 <mamudoubojang17@gmail.com>
2022-04-01 21:55:00 +0000

    this is

--- f.c
+++ f.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include<math.h>
+#define PI 3.142
+// pow(r,a)
+double circumstance(double radius);
+double area(double radius);
 
-float try(float b,float C)
-{ float r;
-	r=a*b;
-	return(r)
+int  main(){
+double c1=5.3, c2=8.9;
+
+
 }
-
-int
