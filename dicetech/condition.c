0d671057d21c657976bba9b20371a80b10a43c29
mamudou1 <mamudoubojang17@gmail.com>
2022-04-22 10:52:38 +0000

    this is for conditions

---
+++ dicetech/condition.c
@@ -0,0 +1,48 @@
+#include<stdarg.h>
+#include<stdlib.h>
+void displaystars();
+int main (){
+void displaystars();
+    int userchoice;
+    printf("Menu\n");
+    printf("1 - &okoro soup\n");
+    printf("2 - kenke  and fish\n");
+    printf("3 - jolof and rice \n");
+    printf("4 - fufu and palnut soup\n");
+    printf("5 - ampesi and palava soup\n");
+void displaystars();
+    printf("choose a menu: \n");
+    scanf("%d",&userchoice);
+
+    if (userchoice==1)
+    {
+       printf("1 - okoro soup\n");
+    }
+    else if (userchoice==2)
+    {
+      printf("2 - kenke  and fish\n");
+    }
+    else if (userchoice==3)
+    {
+         printf("3 - jolof and rice \n");
+    }
+     else if (userchoice==4)
+    {
+        printf("4 - fufu and palnut soup\n");
+    }
+    else if (userchoice==5)
+    {
+         printf("5 - ampesi and palava soup\n");
+        
+    }
+    else{
+         printf("you choose nothing ah why \n");
+    }
+
+    
+ void displaystars();
+ {
+     printf("************************************");
+ }
+    return 0;
+}
