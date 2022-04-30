94d1049d427cf308773de0bbb0378878b56de844
mamudou1 <mamudoubojang17@gmail.com>
2022-04-30 15:17:57 +0000

    this is the lab on switch

---
+++ 3076920.c/condition_for_while.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int a=1, b=1;
+    while (a <=100)
+    {
+        if (a % 2==1)
+        {
+         printf("%d\n",a);   
+        }
+        
+        //printf("A= i will not misbehav in class agin\n");
+        a+=1;
+    }
+    /*
+    do
+    {
+      printf("B= i will not misbehav in class agin\n");
+        b+=1;
+    } while (b % 2==1);
+    
+    */
+   // for multiple table printf("5 X %d = %d", a , (a*5));
+}
