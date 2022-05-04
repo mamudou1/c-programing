da6b4fe052805ff37cce13d30591295f92796be6
mamudou1 <mamudoubojang17@gmail.com>
2022-05-04 17:05:53 +0000

    this is for the 7 assignment

---
+++ forloopass.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+ /*
+    Name 
+    Index Number: 3076920
+    Reference Number: 20454480
+    Program: Bsc Telecommunication Engoineering 
+    Date: 4th May, 2022
+    Task:coding assignment Vii
+    */
+
+int main(){
+    int n;
+for (n=100;n>=1;n--)
+{
+printf ("\n%d bottles of beer on the wall, %d bottles of beer. Take one down and pass it around, %d bottles of beer on the wall.",n,n,(n-1));
+}
+printf("\nNo more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");   
+}
