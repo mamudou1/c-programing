fbb54af545e30549e7186af74b1a6d3d6c4cf04b
mamudou1 <mamudoubojang17@gmail.com>
2022-05-11 17:12:06 +0000

    this is to add the final file to local repository

---
+++ character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    gamestart:   //label
+    goto gamestart;
+    
+    
+    printf("**********************************************\n");
+    printf("###........WHO WANT TO BE A MILLIONAIRE....###\n");
+    printf("**********************************************\n");
+    printf("Question: What is the name of the reator of Bitcoin?\n");
+
+}
