fd3d3c563f0c932296c3511029d12f7b4d150ff1
mamudou1 <mamudoubojang17@gmail.com>
2022-05-30 06:10:44 +0000

    this is for midsem

---
+++ programiz/ano.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void solve() {
+    int a = 3;
+    int res = a++ + ++a + a++ + ++a;
+    printf("%d", res);
+}
+int main() {
+	solve();
+	return 0;
+}
+
+
+
+
---
+++ programiz/intro.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=25;
+    double money =12.9;
+    char b="v";
+    printf("%c",b);
+    printf("%lf",money); 
+    printf("%d",a);
+    return ;
+}
